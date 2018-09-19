a3d152c5010ea0ff11a3d6109a69bdbd2a4a4a66
DeeTee <david.terins@telia.com>
2018-09-19 23:59:03 +0200

    Can now move objects on the map

---
+++ MapTool/Assignment_1a/ViewModels/MapObjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1a.ViewModels
+{
+	public class MapObjectViewModel : ViewModelBase
+	{
+		public double Width { get => 20 ;  }
+		public double Height { get => 20; }
+
+		double _posX = 20;
+		public double PosX { get => _posX; set { _posX = value; OnPropertyChanged(nameof(PosX)); } }
+		double _posY = 20;
+		public double PosY { get => _posY; set { _posY = value; OnPropertyChanged(nameof(PosY)); Console.WriteLine(PosX + ", " + PosY); } }
+
+		double _top = 20;
+		public double Top { get => _top; set { _top = value; OnPropertyChanged(nameof(Top)); } }
+		double _left = 20;
+		public double Left { get => _left; set { _left = value; OnPropertyChanged(nameof(Left)); Console.WriteLine(Top + ", " + Left); } }
+
+	}
+}
---
+++ MapTool/Assignment_1a/ViewModels/MapViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Assignment_1a.ViewModels
+{
+	public class MapViewModel : ViewModelBase
+	{
+		Vector3D _dimensions;
+		public Vector3D Dimensions { get => _dimensions; set { _dimensions = value; OnPropertyChanged(nameof(Dimensions)); } }
+
+		public ObservableCollection<MapObjectViewModel> Items { get; set; }
+
+		float _top = 20;
+		public float Top { get => _top; set { _top = value; OnPropertyChanged(nameof(Top)); } }
+		float _left = 20;
+		public float Left { get => _left; set { _left = value; OnPropertyChanged(nameof(Left)); Console.WriteLine(Top + ", " + Left); } }
+
+		public MapViewModel()
+		{
+			Items = new ObservableCollection<MapObjectViewModel>();
+			Items.Add(new MapObjectViewModel());
+			Items.Add(new MapObjectViewModel());
+		}
+
+	}
+}
